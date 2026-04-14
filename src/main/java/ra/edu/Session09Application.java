492f3da0d78a09b8cf016f410e78d87f03905ad6
yuhd1907 <HuyDN.B23CE045@stu.ptit.edu.vn>
2026-04-14 07:29:10 +0700

    Initial commit

---
+++ src/main/java/ra/edu/Session09Application.java
@@ -0,0 +1,13 @@
+package ra.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Session09Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Session09Application.class, args);
+    }
+
+}
---
+++ src/test/java/ra/edu/Session09ApplicationTests.java
@@ -0,0 +1,13 @@
+package ra.edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Session09ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
